e0e0a6bdbc635f877504918a142270d8162048df
tiagofvx <tiagofvx@gmail.com>
2021-06-18 21:24:50 -0300

    structuring our app

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
 import "fmt"
+// Struct which contains pointers to database connections.
+type App struct {
 
+}
+
+// Run - sets up ou app.
+func (app *App) Run() error  {
+	fmt.Println("Setting up our app!")
+	return nil
+}
 
 func main(){
 	fmt.Println("Go rest api !")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting server!")
+		fmt.Println(err)
+	}
 }
